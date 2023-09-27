b59983c8ac0bad9e0984a9e8247409145e08f0d2
KozichKonstantin <humanstepyt@gmail.com>
2023-09-27 15:17:33 +0300

    27.09.2023  s2
    CREATING

--- tryingBeuty/functions.js
+++ tryingBeuty/functions.js
@@ -1,5 +1,8 @@
 function addCollumn (weight, collumns){
-
+    const collumnHight = weight;
+    const createdType = document.createElement('div');
+    createdType.className = (`column col${collumns +1}`);
+    createdType.style.height = (`${collumnHight}px`);
+    return document.getElementsByClassName('graph')[0].append(createdType)
 }
-const createdType = document.createElement('div');
-return createdType.classList.add(`column col${collumns + 1}`), collumnHight && (createdType.hight = collumnHight), document.getElementsByClassName('graph')[0].append(createdType)
+// как-то так тут есть баг если вводить много колонок и если вводить в инпут e
