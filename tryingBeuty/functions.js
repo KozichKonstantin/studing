00d38c4ff091cabb7382f72e9343db786f33d077
KozichKonstantin <humanstepyt@gmail.com>
2023-09-29 14:55:58 +0300

    30.09.2023 S3
    finished code wars

--- tryingBeuty/functions.js
+++ tryingBeuty/functions.js
@@ -2,7 +2,29 @@ function addCollumn (weight, collumns){
     const collumnHight = weight;
     const createdType = document.createElement('div');
     createdType.className = (`column col${collumns +1}`);
-    createdType.style.height = (`${collumnHight}px`);
+    createdType.style.height = (`${collumnHight-(-editHight(weight))}px`);
     return document.getElementsByClassName('graph')[0].append(createdType)
 }
 // как-то так тут есть баг если вводить много колонок и если вводить в инпут e
+function deleteOld (collumns){
+    console.log(collumns);
+    const allCollumns = document.querySelectorAll('.column');
+    if (collumns == 13){
+        console.log('got it')
+        allCollumns[0].remove();
+    }
+}
+function editHight(weight){
+        if ((weight>0)&&(weight<40)){
+            return 200
+        }
+        if ((weight>=40) && (weight <100)){
+            return 150
+        }
+        if (weight>=100){
+            return 50;
+        }
+        if (weight>400){
+            return (400 - weight);
+        }
+}
--- tryingBeuty/listeners.js
+++ tryingBeuty/listeners.js
@@ -2,4 +2,5 @@ document.getElementsByClassName('submit')[0].addEventListener('click', ()=>{
     let collumns = document.getElementsByClassName('column').length
     let weight = document.getElementsByClassName('addWeight')[0].value
     addCollumn(weight, collumns);
+    deleteOld(collumns)
 })
