4ba86aff23ed69a82bf77ad246328e508018c720
KozichKonstantin <humanstepyt@gmail.com>
2023-09-30 17:10:01 +0300

    30.09.2023 S3
    finished function of sizing

--- tryingBeuty/functions.js
+++ tryingBeuty/functions.js
@@ -1,10 +1,19 @@
 function addCollumn (weight, collumns){
     const collumnHight = weight;
-    let firstCollumn = document.getElementsByClassName('column')[0].style.height
     const createdType = document.createElement('div');
     createdType.className = (`column col${collumns +1}`);
-    createdType.style.height = (`${collumnHight-(-editHight(firstCollumn))}px`);
+    createdType.setAttribute('name', `${weight}`);
+    if(collumns>0){
+        let firstCollumn = document.getElementsByClassName('column')[0].getAttribute('name');
+        createdType.style.height = (`${collumnHight-(-editHight(firstCollumn))}px`)
+        // console.log(firstCollumn, 'firstcollumn');
     return document.getElementsByClassName('graph')[0].append(createdType)
+    }else{
+        let firstCollumn = weight;
+        createdType.style.height = (`${collumnHight-(-editHight(firstCollumn))}px`);
+        return document.getElementsByClassName('graph')[0].append(createdType)
+    }
+    
 }
 // как-то так тут есть баг если вводить много колонок и если вводить в инпут e
 function deleteOld (collumns){
@@ -13,6 +22,7 @@ function deleteOld (collumns){
     if (collumns == 13){
         console.log('got it')
         allCollumns[0].remove();
+        editAppend(collumns);
     }
 }
 function editHight(weight){
@@ -32,4 +42,13 @@ function editHight(weight){
             console.log('-x')
             return (400 - weight);
         }
+}
+function editAppend (collumns){
+    for(let i = 0; i < collumns; i++){
+        let allCollumns = document.getElementsByClassName('column');
+        let colHight = allCollumns[i].getAttribute('name');
+        let firstColHi = allCollumns[0].getAttribute('name');
+        allCollumns[i].style.height = (`${colHight - (-editHight(firstColHi))}px`)
+        
+    }
 }
