5f629f3743be8fc0444f20f8e05b38fa75b4ce7a
KozichKonstantin <humanstepyt@gmail.com>
2023-09-30 11:38:41 +0300

    30.09.2023 S3
    have an idea

--- tryingBeuty/functions.js
+++ tryingBeuty/functions.js
@@ -1,8 +1,9 @@
 function addCollumn (weight, collumns){
     const collumnHight = weight;
+    let firstCollumn = document.getElementsByClassName('column')[0].style.height
     const createdType = document.createElement('div');
     createdType.className = (`column col${collumns +1}`);
-    createdType.style.height = (`${collumnHight-(-editHight(weight))}px`);
+    createdType.style.height = (`${collumnHight-(-editHight(firstCollumn))}px`);
     return document.getElementsByClassName('graph')[0].append(createdType)
 }
 // как-то так тут есть баг если вводить много колонок и если вводить в инпут e
@@ -16,15 +17,19 @@ function deleteOld (collumns){
 }
 function editHight(weight){
         if ((weight>0)&&(weight<40)){
+            console.log('+200')
             return 200
         }
         if ((weight>=40) && (weight <100)){
+            console.log('+150')
             return 150
         }
         if (weight>=100){
+            console.log('+50')
             return 50;
         }
         if (weight>400){
+            console.log('-x')
             return (400 - weight);
         }
 }
