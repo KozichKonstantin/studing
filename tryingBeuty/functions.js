8c6316202e445b65699d2c91b79a5ebfc291ee55
KozichKonstantin <humanstepyt@gmail.com>
2023-10-04 23:59:26 +0300

    04.10.2023 S5
    working with styles

--- tryingBeuty/functions.js
+++ tryingBeuty/functions.js
@@ -1,22 +1,27 @@
 function addCollumn (weight, collumns){
     const createdType = document.createElement('div');
-    createdType.className = (`column col${collumns +1}`);
+    console.log(collumns)
+    createdType.className = (`column`);
     createdType.setAttribute('name', `${weight}`);
     createdType.style.height = (`${weight}px`)
     document.getElementsByClassName('collumsContent')[0].append(createdType)
     
 }
 function addDate(date, collumns){
+    console.log(collumns, ' addDate col');
     const createdDate = document.createElement('p');
-    createdDate.className = (`date col${collumns+1}`);
+    createdDate.className = (`date`);
     createdDate.textContent = `${date}`;
     document.getElementsByClassName('datesContent')[0].append(createdDate)
 }
 /* global */
 function deleteOld (collumns, stoper){
     const allCollumns = document.querySelectorAll('.column');
+    const allDates = document.querySelectorAll('.date');
     if (collumns == stoper){
+        console.log(allCollumns[0].classList)
         allCollumns[0].remove();
+        allDates[0].remove();
     }
 }
 /** */
@@ -52,17 +57,15 @@ function buildNavigation(){
     document.getElementsByClassName('navigation')[0].append(navigation);
     document.getElementsByClassName('navigation')[0].setAttribute('name', 'added');
     for(let i = 0; i < 4; i++){
-        console.log('append')
         navigation = document.createElement('div');
         navigation.className = 'button';
         document.getElementsByClassName('navCase')[0].append(navigation);
     }
 }
 function deleteNavigation(){
-    document.getElementsByClassName('navigation')[0].setAttribute('name', 'deleted')
+    document.getElementsByClassName('navigation')[0].setAttribute('name', 'deleted');
     function del(){
         document.getElementsByClassName('navCase')[0].remove();
     }
-    setTimeout(del, 450)
-    
+    setTimeout(del, 450);
 }
--- tryingBeuty/listeners.js
+++ tryingBeuty/listeners.js
@@ -2,18 +2,19 @@ let objMass = [];
 document.getElementsByClassName('submit')[0].addEventListener('click', ()=>{
     let collumns = document.getElementsByClassName('column');
     let weight = document.getElementsByClassName('addWeight')[0].value;
+    if (weight==0){
+        return console.error('cant add collumn')
+    }
     let date = document.getElementsByClassName('addDate')[0].value;
     let coll = new CollumnWithF()
-    try{coll.weight = (weight-1+1);}
-    catch (error){
-        console.error('cant add Collumn')
-    }
+    coll.weight = (weight-1+1);
     coll.date = date;
     coll.number = collumns.length++;
+    console.log(coll.number)
     addCollumn(coll.weight, collumns.length);
     addDate(coll.date, collumns.length)
     objMass.push(coll);
-    let stoper = 13;
+    let stoper = 14;
     deleteOld(collumns.length, stoper);
     if(objMass.length == stoper){
         objMass=objMass.slice(1)
