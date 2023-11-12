0d491678bccea01aabd2eb7c1d825f4a4a428162
KozichKonstantin <humanstepyt@gmail.com>
2023-11-12 22:18:30 +0300

    12.11.2023 S7
    made appending function

--- graph/scripts/functions.js
+++ graph/scripts/functions.js
@@ -1,48 +1,52 @@
 import { Graph, GraphElement } from "./Classes.js";
-const newGrahp = new Graph([])
-function circuitColors(iterator){
-    const colors = [
-        'rgb(31, 119, 119)',
-        'rgb(31, 72, 119)',
-        'rgb(35, 31, 119)',
-        'rgb(81, 31, 119)',
-        'rgb(118, 31, 119)',
-        'rgb(119, 31, 72)',
-        'rgb(119, 31, 31)',
-        'rgb(119, 109, 31)',
-        'rgb(72, 119, 31)',
-        'rgb(31, 119, 66)'
-    ]
-    if(iterator > colors.length-1){
-        iterator  = iterator - (Math.trunc(iterator / colors.length))*colors.length
-    }
-    return colors[iterator]
+const newGrahp = new Graph([]);
+function circuitColors(iterator) {
+  const colors = [
+    "rgb(31, 119, 119)",
+    "rgb(31, 72, 119)",
+    "rgb(35, 31, 119)",
+    "rgb(81, 31, 119)",
+    "rgb(118, 31, 119)",
+    "rgb(119, 31, 72)",
+    "rgb(119, 31, 31)",
+    "rgb(119, 109, 31)",
+    "rgb(72, 119, 31)",
+    "rgb(31, 119, 66)",
+  ];
+  if (iterator > colors.length - 1) {
+    iterator = iterator - Math.trunc(iterator / colors.length) * colors.length;
+  }
+  return colors[iterator];
 }
-export function createElement(cost, connections, name){
-    let color = circuitColors(newGrahp.elements.length)
-    connections = connections.split(', ')
-    let place = document.getElementsByClassName('graph')[0];
-    let element = document.createElement('div')
-    element.className = `graphElement ${name}`;
-    let newElement = new GraphElement(element, cost, connections, name, color);
-    newElement.getColor(color);
-    newGrahp.addElement(newElement);
-    place.append(element);
-    place = element;
-    element = document.createElement('div');
-    element.textContent = name;
-    element.style.backgroundColor = 'white';
-    let connectedElements = newGrahp.getConnections(newElement);
-    place.append(element);
-    element = document.createElement('div');
-    element.className = 'connections';
-    place.append(element);
-    place = element;
-    for(let i = 0; i < connectedElements.length; i++){
-        let connectPoint = document.createElement('div');
-        connectPoint.className = 'connectPoint';
-        connectPoint.style.backgroundColor = connectedElements[i].color;
-        place.append(connectPoint);
-        console.log(connectedElements[i].color);
-    }
+function appendElement(type, place, elemClass = "", name = "", value = "") {
+  let element = document.createElement(type);
+  let appendPlace = document.getElementsByClassName(place)[0];
+  element.className = elemClass;
+  if (name != "") {
+    element.setAttribute("name", name);
+  }
+  if (value != "") {
+    element.value = value;
+  }
+  appendPlace.append(element);
+  return element;
+}
+export function createElement(cost, connections, name) {
+  let color = circuitColors(newGrahp.elements.length);
+  connections = connections.split(", ");
+  let element = appendElement('div', 'graph', `graphElement ${name}`)
+  let newElement = new GraphElement(element, cost, connections, name, color);
+  newElement.getColor(color);
+  newGrahp.addElement(newElement);
+  let place = element.classList;
+  element = appendElement('div', place, `elementName ${name}`)
+  element.textContent = name;
+  element.style.backgroundColor = "white";
+  let connectedElements = newGrahp.getConnections(newElement);
+  place = appendElement('div', place, `connections ${name}`)
+  for (let i = 0; i < connectedElements.length; i++) {
+    console.log(place.classList)
+    let connectPoint = appendElement('div', place.className, 'connectPoint')
+    connectPoint.style.backgroundColor = connectedElements[i].color;
+  }
 }
