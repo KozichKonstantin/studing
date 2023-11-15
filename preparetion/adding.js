0b073105449f3b01c59830fadb349ddb1382ddbb
KozichKonstantin <humanstepyt@gmail.com>
2023-11-15 23:40:43 +0300

    15.11.2023 S7
    trying to create universl function of creating connections lines

--- preparetion/adding.js
+++ preparetion/adding.js
@@ -1,23 +1,33 @@
-function appendElement(
-    type,
-    place,
-    elemClass = "",
-    name = "",
-    value = "",
-    ret = 0
-  ) {
-    let element = document.createElement(type);
-    let appendPlace = document.getElementsByClassName(place)[0];
-    element.className = elemClass;
-    if (name != "") {
-      element.setAttribute("name", name);
-    }
-    if (value != "") {
-      element.value = value;
-    }
-    if ((ret == 0)) {
-      appendPlace.append(element);
-    }
-    return element;
-  }
-appendElement('div', 'graph', 'elem')
+const colors = [
+  'green', 
+  'blue', 
+  'red',
+  'yellow',
+  'black',
+  'pink',
+  'white'
+]
+function addElemet(type, place, className, background){
+  let element = document.createElement(type)
+  element.className = className;
+  element.style.backgroundColor = background 
+  place.append(element)
+  return element
+}
+function createLine(fromElement, toElement){
+  let line = document.createElement('hr')
+  let place = document.getElementsByClassName('graph')[0]
+  line.style.height = coordinates[toElement][1] - coordinates[fromElement][1] -20
+  line.style.transform = `translateY(${(coordinates[toElement][1] + coordinates[fromElement][1])/2 -90}px)`
+  console.log((coordinates[toElement][1] - coordinates[fromElement][1])/2)
+  place.append(line)
+}
+let coordinates = [];
+let graph = document.getElementsByClassName('graph')[0]
+for(let i = 0; i< colors.length; i++){
+  let element = addElemet('div', graph, 'graphElement', colors[i])
+  coordinates.push([element.getBoundingClientRect().x, element.getBoundingClientRect().y])
+}
+createLine(0, 1)
+createLine(1, 2)
+console.log(coordinates)
