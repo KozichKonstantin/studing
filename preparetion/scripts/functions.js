9b27c157b2cb0c8dfe53672ba580402242d8b323
KozichKonstantin <humanstepyt@gmail.com>
2023-11-16 14:57:29 +0300

    16.11.2023 S7
    now line is rotating

--- preparetion/scripts/functions.js
+++ preparetion/scripts/functions.js
@@ -21,12 +21,14 @@ const colors = [
     let place = document.getElementsByClassName('graph')[0]
     line.style.height = toElement.coordinates[1] - fromElement.coordinates[1]
     line.style.transform = `translateY(${(fromElement.coordinates[1] + toElement.coordinates[1])/2 -80}px)`
+    line.style.rotate = `${-((toElement.coordinates[0] - fromElement.coordinates[0])/(toElement.coordinates[1] - fromElement.coordinates[1]))*90}deg`
     place.append(line)
   }
   const GraphObj = new Graph;
   let graphHtml = document.getElementsByClassName('graph')[0]
   for(let i = 0; i< colors.length; i++){
     let element = addElemet('div', graphHtml, 'graphElement', colors[i])
+    element.style.marginLeft = `${-40 * i}px`
     let newElement = new GraphElement(element, [element.getBoundingClientRect().x, element.getBoundingClientRect().y], colors[i])
     GraphObj.addElement(newElement);
   }
