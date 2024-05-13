a06784b1d827530f8aa7381ec6c26cb5c2ca3b5a
KozichKonstantin <humanstepyt@gmail.com>
2024-05-13 14:50:52 +0300

    13.05.24
    working on algorithm of IMT

--- tryingBeuty/app.js
+++ tryingBeuty/app.js
@@ -1,28 +1,37 @@
-const express = reqire('express');
-const http = require('http');
-const fs = require('fs');
+import { UserParams } from "./algotithm.js";
+const express = require("express");
+const http = require("http");
+const fs = require("fs");
 const app = express();
 const Port = 3000;
-const mysql = require('mysql');
-const urlencodedParcer = express.urlencodedParcer({extended:false});
+const mysql = require("mysql");
+const urlencodedParcer = express.urlencodedParcer({ extended: false });
 // import connection from './connection';
 const connect = mysql.createConnection({
-    connection
-})
-app.listen(Port, (error)=>{
-    error ? console.error('error to connect') : console.log(`listening port: ${Port}`);
-
+  connection,
 });
-app.set('view engine', 'ejs');
-app.get('/', (req,res)=>{
-    res.render('index');
-})
-app.get('/userPage', (req,res)=>{
-    res.render('userPage');
-})
-app.get('/userPage/login', (req, res)=>{
-    res.render('login');
-})
-app.post('/userPage/login', urlencodedParcer ,(req, res)=>{
-    res.end;
-})
+app.listen(Port, (error) => {
+  error
+    ? console.error("error to connect")
+    : console.log(`listening port: ${Port}`);
+});
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/userPage", (req, res) => {
+  console.log("huy");
+  res.render("userPage");
+});
+app.get("/userParams", (req, res) => {
+  let user1 = new UserParams(90, 180, 22, [1, 1]);
+  user1.GetIMT();
+  user1.GetParams();
+  res.render("userPage");
+});
+app.get("/userPage/login", (req, res) => {
+  res.render("login");
+});
+app.post("/userPage/login", urlencodedParcer, (req, res) => {
+  res.end;
+});
