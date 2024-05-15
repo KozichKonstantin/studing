c51c7bd135bb9b7ad7d27e92e54358c2a9381d51
KozichKonstantin <humanstepyt@gmail.com>
2024-05-16 01:09:36 +0300

    16.05.24 Little work on algorithm of training
    wrote first iteration of algorithm for choosing training program
    (not tested)

--- tryingBeuty/views/scripts/algotithm.js
+++ tryingBeuty/views/scripts/algotithm.js
@@ -1,8 +1,7 @@
-let weight;
-let height;
-let age;
 const basicAge = 65;
 const basicIMT = 22;
+const lineDlAge = [-42, -30, -15, 0, 15, 20, 30]
+const lineDlIMT = [-21, -17, -12, 0, 8, 13, 18]
 export class UserParams {
   constructor(weight, height, age, params) {
     this.weight = weight;
@@ -10,17 +9,36 @@ export class UserParams {
     this.age = age;
     this.params = params;
   }
-  GetIMT(weight, height) {
+  GetIMT() {
     let IMT = (this.weight / (this.height / 100)) ^ 2;
     this.IMT = IMT;
     return IMT;
   }
   GetParams() {
-    let deltaIMT = this.IMT - basicIMT;
-    let deltaAge = this.age - basicAge;
-    this.params = [deltaAge, deltaIMT];
+    this.deltaIMT = this.IMT - basicIMT;
+    this.deltaAge = this.age - basicAge;
+    this.params = [this.deltaAge, this.deltaIMT];
     console.log(this.params);
     return this.params;
+  }
+  GetProgram(){
+    let imtQ = 0;
+    let ageQ = 0;
+    for(let i = 0; i <= 7; i++){
+      if(this.params[0] >= lineDlAge[i]){
+        i++
+      }else{
+        imtQ = i;
+      }
+    }
+    for(i = 0; i<=7; i++){
+      if(this.params[1] >= lineDlIMT[i]){
+      }else{
+        ageQ = i;
+      }
+    }
+    let trainPlace = [ageQ, imtQ]
+    return trainPlace;
   }
 }
 let user1 = new UserParams(90, 180, 22, [1, 1]);
