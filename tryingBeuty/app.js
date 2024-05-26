4d0b6ebdad86813e6a146ce0b0e796a72b26cfea
KozichKonstantin <humanstepyt@gmail.com>
2024-05-26 20:05:02 +0300

    26.05.24 authorization & registration

--- tryingBeuty/app.js
+++ tryingBeuty/app.js
@@ -12,7 +12,7 @@ app.use(express.static(`${__dirname}/views`));
 // import connection from './connection.js';
 const connection = mysql.createConnection({
   host: "localhost",
-  database: "fitnes",
+  database: "fitnesdb",
   user: "siteFindigBase",
   password: "humansedrexstep",
   port: "3306",
@@ -41,28 +41,36 @@ app.get("/userPage/login", (req, res) => {
 app.get("/regPage", (req, res) => {
   res.render("regPage");
 });
-app.post("/userPage/login", urlencondedParcer, (req, res) => {
-  res.end;
+app.get("/loginPage", (req, res)=>{
+  res.render("login")
+})
+app.post(
+  "/registrationPage/registrationSucces",
+  urlencondedParcer,
+  (req, res) => {
+    let registrateNew = `INSERT INTO user (username, password) VALUES ( '${req.body.username}', '${req.body.password}')`;
+    let addInfo = `INSERT INTO userinfo(age, tall) VALUES ( '${req.body.age}', '${req.body.tall}')`;
+    connection.connect(function (err) {
+      if (err) {
+        return console.log("blya pizdec");
+      } else {
+        console.log("ne pizdec");
+      }
+    });
+    connection.query(registrateNew, (err, result) => {
+      console.log(err);
+      console.log("added user name =", req.body.login);
+    });
+    connection.query(addInfo, (err, result) => {
+      console.log(err);
+    });
+    res.redirect("/");
+  }
+);
+app.get("/choosingPage", (req, res) => {
+  res.render("choosingPage");
 });
-app.post("/regPage/registrationSucces", (req, res) => {
-  let registrateNew = `INSERT INTO user (username, password, e-mail) VALUES ( '${req.body.login}', '${req.body.password}', '${req.body.email}' )`;
-  let addInfo = `INSERT INTO userinfo(fullname, birthdate,	e-mail,	sex,	tall,	weightBase) VALUES ( '${req.body.fullname}', '${req.body.birthdate}', '${req.body.e-mail}', '${req.body.sex}', '${req.body.tall}', '${req.body.weightBase}' )`;
-  connection.connect(function (err) {
-    if (err) {
-      return console.log("blya pizdec");
-    } else {
-      console.log("ne pizdec");
-    }
-  });
-  connection.query(registrateNew, (err, result) => {
-    console.log(err);
-    console.log("added user name =", req.body.login);
-  });
-  connection.query(addInfo, (err, result)=>{
-    console.log(err);
-  })
-  res.redirect("/firstPage");
-});
+
 app.post("/login/saveLogin", jsonParser, (req, res) => {
   let selectMore = `SELECT password FROM user WHERE login = '${req.body.login}'`;
   connection.connect(function (err) {
@@ -87,4 +95,41 @@ app.post("/login/saveLogin", jsonParser, (req, res) => {
       res.end;
     }
   });
+});
+app.post("/login/loginSucces", urlencondedParcer, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+
+  connection.connect(function (err) {
+    if (err) {
+      return console.log("blya pizdec");
+    } else {
+      console.log("ne pizdec");
+    }
+  });
+
+  let select = `SELECT password FROM user WHERE username = '${req.body.username}'`;
+  connection.connect(select, (err, result) => {
+    console.log(err, 'error is');
+    if(err){
+      res.redirect('/loginPage')
+      res.end()
+    } else{
+      if(result[0] == undefined || result[0] == null){
+        res.render("login");
+        res.end()
+      }else{
+      // console.log(typeof result[0].password);
+      if (result[0].password == `${req.body.password}`) {
+        res.redirect("/");
+        res.end()
+      } else {
+        res.render("login");
+        res.end();
+      }
+    }
+    }
+    // console.log("/////////", result[0]);
+    
+});
+connection.end();
 });
